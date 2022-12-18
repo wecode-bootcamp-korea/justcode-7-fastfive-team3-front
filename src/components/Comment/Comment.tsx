5b411c675a528c103f0b596c55441bb1ff7ad131
DawonOh <dawonohh@gmail.com>
2022-12-19 08:37:04 +0900

    Add: 댓글  입력창 레이아웃 구현

--- src/components/Comment/Comment.tsx
+++ src/components/Comment/Comment.tsx
@@ -5,6 +5,7 @@ import css from './Comment.module.scss';
 const Comment = () => {
   //글자 수
   const [replyTextLength, setReplyTextLength] = useState(0);
+  const [replyMainTextLength, setReplyMainTextLength] = useState(0);
 
   //textarea 처음에 비활성화 -> 수정 클릭 시 활성화
   const [isMyTextarea, setIsMyTextarea] = useState(true);
@@ -23,7 +24,7 @@ const Comment = () => {
       alert('취소되었습니다.');
     }
   };
-  //비밀 여부
+  //답글 비밀 여부
   const [isSecret, setIsSecret] = useState(false);
   const setSecret = () => {
     setIsSecret(!isSecret);
@@ -32,7 +33,7 @@ const Comment = () => {
   //rnk : 0이면 일반댓글
   //rnk : 1이면 일반댓글
 
-  //등록 버튼 활성화 여부
+  //답글 등록 버튼 활성화 여부
   const textareaDOM = useRef<HTMLTextAreaElement>(null);
   const textareaValue = textareaDOM.current?.value;
   const [isDisable, setIsDisable] = useState(true);
@@ -43,7 +44,6 @@ const Comment = () => {
       setIsDisable(true);
     }
   }, [textareaValue]);
-
   const handleResizeHeight = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     //textarea 내용에 따른 높이 변경
     e.target.style.height = '1px';
@@ -56,6 +56,41 @@ const Comment = () => {
       setReplyTextLength(0);
     }
   };
+
+  //메인 비밀 여부
+  const [isMainSecret, setMainIsSecret] = useState(false);
+  const setMainSecret = () => {
+    setMainIsSecret(!isMainSecret);
+  };
+
+  //메인 등록 버튼 활성화 여부
+  const [isMainDisable, setIsMainDisable] = useState(true);
+  const mainTextareaDOM = useRef<HTMLTextAreaElement>(null);
+  const mainTextareaValue = mainTextareaDOM.current?.value;
+  console.log(mainTextareaValue);
+  useEffect(() => {
+    if (mainTextareaValue) {
+      setIsMainDisable(false);
+    } else {
+      setIsMainDisable(true);
+    }
+  }, [mainTextareaValue]);
+  const handleMainResizeHeight = (
+    e: React.ChangeEvent<HTMLTextAreaElement>
+  ) => {
+    //textarea 내용에 따른 높이 변경
+    e.target.style.height = '1px';
+    e.target.style.height = e.target.scrollHeight + 'px';
+    //글자수 count
+    const currentTextareaText = e.target.value;
+    if (currentTextareaText) {
+      setReplyMainTextLength(currentTextareaText.length);
+    } else if (!currentTextareaText) {
+      setReplyMainTextLength(0);
+    }
+  };
+  console.log('MainDisable : ', isMainDisable);
+
   return (
     <div className={css.commentContainer}>
       <h1 className={css.commentTitle}>댓글</h1>
@@ -154,6 +189,29 @@ const Comment = () => {
             }
             disabled
           />
+        </div>
+        <div>페이지네이션~</div>
+        <div className={`${css.gridItem} ${css.mainReply}`}>
+          <textarea
+            className={css.commentContent}
+            placeholder="위 멤버에게 궁금한 점이나 제안하고 싶은 내용을 댓글로 남겨보세요."
+            ref={mainTextareaDOM}
+            rows={1}
+            onInput={handleMainResizeHeight}
+          />
+          <div className={css.countAndsend}>
+            <span className={css.count}>{replyMainTextLength}</span>/1000
+            <div
+              className={isMainSecret ? css.lock : css.unlock}
+              onClick={setMainSecret}
+            />
+            <button
+              className={isMainDisable ? css.notSendReply : css.sendReply}
+              disabled={isMainDisable}
+            >
+              등록
+            </button>
+          </div>
         </div>
       </div>
     </div>
