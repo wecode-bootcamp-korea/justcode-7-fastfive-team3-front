eebc898f21434697a04ee944be84355bdffad797
DawonOh <dawonohh@gmail.com>
2022-12-27 15:31:43 +0900

    Add: 'oo님 안녕하세요' 헤더에 문구 추가

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,6 +4,7 @@ import css from './Header.module.scss';
 
 const Header = () => {
   const token = localStorage.getItem('token');
+  const nickName = localStorage.getItem('nickname');
   const logout = () => {
     localStorage.clear();
   };
@@ -12,15 +13,18 @@ const Header = () => {
       <h1>
         <Link to="/subHome">FASTFIVE</Link>
       </h1>
-      {token ? (
-        <button onClick={logout}>
-          <Link to="/">로그아웃</Link>
-        </button>
-      ) : (
-        <button>
-          <Link to="/">로그인</Link>
-        </button>
-      )}
+      <div className={css.headerLeft}>
+        <p>{nickName}님 안녕하세요!</p>
+        {token ? (
+          <button onClick={logout}>
+            <Link to="/">로그아웃</Link>
+          </button>
+        ) : (
+          <button>
+            <Link to="/">로그인</Link>
+          </button>
+        )}
+      </div>
     </header>
   );
 };
